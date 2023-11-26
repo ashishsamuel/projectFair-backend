db5a81ec38bd6764ab0d4cba71ab2a66c4f9a07c
ashish <ashishsamuelthomson@gmail.com>
2023-11-26 20:44:06 +0530

    add and update project api backend completed

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -40,8 +40,12 @@ exports.allUserProjects = async(req,res)=>{
 
 // getallprojects - token required
 exports.getallProjects = async(req,res)=>{
+    const searchKey = req.query.search
+    const query = {
+        languages:{$regex:searchKey , $options:"i"}
+    }
     try{
-        const allProjects = await projects.find()
+        const allProjects = await projects.find(query)
         res.status(200).json(allProjects)
     }catch(err){
         res.status(401).json(err)
@@ -56,4 +60,23 @@ exports.getHomeProjects = async(req,res)=>{
     }catch(err){
         res.status(401).json(err)
     }
+}
+
+// edit project
+exports.editProject = async (req,res)=>{
+    // get project id
+    const {id} = req.params;
+    const {userId} = req.payload;
+    const {title,languages,overview,github,website,projectImage} = req.body
+    const uploadProjectImage = req.file?req.file.filename:projectImage
+    try{
+        const updateProject = await projects.findByIdAndUpdate({"_id":id},{title,languages,overview,
+        github,website,projectImage:uploadProjectImage,userId
+        },{new:true})
+        await updateProject.save()
+        res.status(200).json(updateProject)
+    }catch(err){
+        res.status(401).json(err)
+    }
+
 }
--- Routes/router.js
+++ Routes/router.js
@@ -5,6 +5,7 @@ const userController = require('../Controllers/userController')
 const projectController = require('../Controllers/projectController');
 const jwtMiddleware = require('../Middlewares/jwtMiddleware');
 const multerConfig = require('../Middlewares/multerMiddleware');
+const multer = require('multer');
 
 // register API
 router.post('/user/register',userController.register)
@@ -18,6 +19,8 @@ router.get('/user/all-projects',jwtMiddleware,projectController.allUserProjects)
 router.get('/projects/all',jwtMiddleware,projectController.getallProjects)
 // gethomeprojects
 router.get('/projects/home-projects',projectController.getHomeProjects)
+// edit project
+router.put('/project/edit/:id',jwtMiddleware,multerConfig.single('projectImage'),projectController.editProject)
 
 // to provide the path of the index.js we need to export the router since indes.js is the only file that will run in the server 
 module.exports = router
